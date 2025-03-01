c46a4f6ba72d30256bf3fc8d6856350927281058
unknown <raflisilehu20@gmail.com>
2025-03-01 09:32:58 +0700

    fix(common): fix some bugs

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -44,14 +44,14 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
         debug {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
